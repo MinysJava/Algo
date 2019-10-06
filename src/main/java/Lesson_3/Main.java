394e8b079a615c348830f66bb0cf9c0ffb324b66
MinysJava <poforli@gmail.com>
2019-10-06 20:51:34 +0500

    Домашнее задание №3

--- src/main/java/Lesson_3/Main.java
+++ src/main/java/Lesson_3/Main.java
@@ -2,27 +2,14 @@ package Lesson_3;
 
 public class Main {
     public static void main(String[] args) {
-//        MyStack<Integer> stack = new MyStack<>();
-//
-//        stack.push(1);
-//        stack.push(2);
-//        stack.push(3);
-//        stack.push(4);
-//        stack.push(5);
-//
-//        for (int i = 0; i < 5; i++) {
-//            System.out.println(stack.pop());
-//
-//        }
-//        Expreesion e = new Expreesion("(4-5) + [34-{2-3}]");
-//        System.out.println(e.cheakBracket());
 
-        MyQueue<Integer> queue = new MyQueue<>(5);
-        for (int i = 0; i < 5; i++) {
-            queue.insert(i);
-        }
-        for (int i = 0; i < 5; i++) {
-            System.out.println(queue.remove());
+        MyDeque<Integer> deque = new MyDeque<>(5);
+        deque.insertLeft(0);
+        deque.insertLeft(-1);
+        deque.insertRight(1);
+        while( !deque.isEmpty() ){
+            int n = deque.removeLeft();
+            System.out.println(n);
         }
     }
 }
