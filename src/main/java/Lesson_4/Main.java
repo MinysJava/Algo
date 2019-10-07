b27cd583a3a792fab017c37915e55484ca3cac4a
MinysJava <poforli@gmail.com>
2019-10-07 23:08:29 +0500

    Домашнее задание №4

--- src/main/java/Lesson_4/Main.java
+++ src/main/java/Lesson_4/Main.java
@@ -1,5 +1,7 @@
 package Lesson_4;
 
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
         MyLinkedList<String> list = new MyLinkedList<>();
@@ -8,12 +10,17 @@ public class Main {
         list.insertFirst("2");
         list.insertFirst("3");
 
-        System.out.println(list);
-        System.out.println(list.getFirst());
-        System.out.println(list.removeFirst());
+//        System.out.println(list);
+//        System.out.println(list.getFirst());
+//        System.out.println(list.removeFirst());
+//        System.out.println(list);
+
+        list.insertLast("4");
         System.out.println(list);
 
-        list.insert("4", 0);
-        System.out.println(list);
+        Iterator<String> iterator = list.iterator();
+        while (iterator.hasNext()){
+            System.out.println(iterator.next());
+        }
     }
 }
