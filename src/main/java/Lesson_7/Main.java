69ee247564156a3f92eae2564687d98a2e0701f8
MinysJava <poforli@gmail.com>
2019-10-16 22:41:51 +0500

    Урок №7

---
+++ src/main/java/Lesson_7/Main.java
@@ -0,0 +1,13 @@
+package Lesson_7;
+
+public class Main {
+    public static void main(String[] args) {
+        Graph graph = new Graph(5);
+
+        graph.addEdge(1,2);
+        graph.addEdge(0,4);
+        graph.addEdge(1,4);
+
+        System.out.println(graph.getAdjList(1));
+    }
+}
