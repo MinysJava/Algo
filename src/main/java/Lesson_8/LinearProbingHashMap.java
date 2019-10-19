c72e24dd19f7859d9cfe9aab7af2844977075d1c
MinysJava <poforli@gmail.com>
2019-10-19 13:38:47 +0500

    Урок №8

---
+++ src/main/java/Lesson_8/LinearProbingHashMap.java
@@ -0,0 +1,56 @@
+package Lesson_8;
+
+public class LinearProbingHashMap<Key, Value> {
+    private int capacity = 7;
+    private int size = 0;
+
+    private Key[] keys = (Key[]) new Object[capacity];
+    private Value[] values = (Value[]) new Object[capacity];
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty(){
+        return size == 0;
+    }
+
+    public int hash(Key key){
+        return (key.hashCode() & 0x7fffffff) % capacity;
+    }
+
+    public void put(Key key, Value value){
+        isKeyNotNull(key);
+        if(size == capacity - 1){
+            throw new IllegalArgumentException("Array full");
+        }
+        int i;
+        for (i = hash(key); keys[i] != null; i = (i + 1) % capacity) {
+            if(keys[i].equals(key)){
+                values[i] = value;
+                return;
+            }
+        }
+        keys[i] = key;
+        values[i] = value;
+        size++;
+    }
+
+    public Value get (Key key){
+        isKeyNotNull(key);
+        int i;
+        for (i = hash(key); keys[i] != null; i = (i + 1) % capacity) {
+            if(keys[i].equals(key)){
+               return values[i];
+            }
+        }
+        return null;
+    }
+
+    private boolean isKeyNotNull (Key key){
+        if(key == null){
+            throw new IllegalArgumentException("Null is BAD!!");
+        }
+        return true;
+    }
+}
